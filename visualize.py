e0a82a948b07b8bbff71a9cafe183cf25f02443f
default <default@maastrichtuniversity.nl>
2022-05-09 14:17:36 +0000

    visualization and plots

---
+++ visualize.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+loader_CP = np.load('data-arrays/dataset_CP_train_2.npz')
+loader_NCP = np.load('data-arrays/dataset_NCP_train_2.npz')
+loader_Normal = np.load('data-arrays/dataset_Normal_train_2.npz')
+
+dataset_CP = loader_CP['arr_0'] # 1176
+dataset_NCP = loader_NCP['arr_0'] # 1280
+dataset_Normal = loader_Normal['arr_0'] # 850
+
+print('data loaded')
+
+fig = plt.figure(figsize=(12, 12))
+columns = 8
+rows = 4
+scan = dataset_Normal[100]
+i = 1
+for img in scan:
+    img = img * 255
+    fig.add_subplot(rows, columns, i)
+    plt.imshow(img)
+    i += 1
+    
+plt.show()
