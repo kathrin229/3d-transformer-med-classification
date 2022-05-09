2e139dc2a174efbf7ba258f39a4e8a3242504412
default <default@maastrichtuniversity.nl>
2022-05-09 14:18:02 +0000

    reshaping of data

--- cnn-model.py
+++ cnn-model.py
@@ -3,17 +3,22 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import matplotlib.pyplot as plt
 
 # https://keras.io/examples/vision/3D_image_classification/#loading-data-and-preprocessing
-loader_CP = np.load('data-arrays/dataset_CP.npz')
-loader_NCP = np.load('data-arrays/dataset_NCP.npz')
-loader_Normal = np.load('data-arrays/dataset_Normal.npz')
+loader_CP = np.load('data-arrays/dataset_CP_train_2_scaled.npz')
+loader_NCP = np.load('data-arrays/dataset_NCP_train_2_scaled.npz')
+loader_Normal = np.load('data-arrays/dataset_Normal_train_2_scaled.npz')
 
 dataset_CP = loader_CP['arr_0'] # 1176
 dataset_NCP = loader_NCP['arr_0'] # 1280
 dataset_Normal = loader_Normal['arr_0'] # 850
 
 print('data loaded')
+
+dataset_CP = dataset_CP.reshape(-1, 118, 160, 32)
+dataset_NCP = dataset_NCP.reshape(-1, 118, 160, 32)
+dataset_Normal = dataset_Normal.reshape(-1, 118, 160, 32)
 
 CP_labels = np.array([[1,0,0] for _ in range(len(dataset_CP))])
 NCP_labels = np.array([[0,1,0] for _ in range(len(dataset_NCP))])
@@ -49,21 +54,21 @@ def get_model(width=128, height=128, depth=50):
 
     inputs = keras.Input((width, height, depth))
 
-    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(inputs)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(inputs)
     x = layers.MaxPool2D(pool_size=2)(x)
     x = layers.BatchNormalization()(x)
 
-    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
     x = layers.MaxPool2D(pool_size=2)(x)
     x = layers.BatchNormalization()(x)
 
-    x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
-    x = layers.MaxPool2D(pool_size=2)(x)
-    x = layers.BatchNormalization()(x)
+    # x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+    # x = layers.MaxPool2D(pool_size=2)(x)
+    # x = layers.BatchNormalization()(x)
 
-    x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
-    x = layers.MaxPool2D(pool_size=2)(x)
-    x = layers.BatchNormalization()(x)
+    # x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+    # x = layers.MaxPool2D(pool_size=2)(x)
+    # x = layers.BatchNormalization()(x)
 
     x = layers.GlobalAveragePooling2D()(x)
     x = layers.Dense(units=512, activation="relu")(x)
@@ -77,7 +82,7 @@ def get_model(width=128, height=128, depth=50):
 
 
 # Build model.
-model = get_model(width=50, height=128, depth=128)
+model = get_model(width=118, height=160, depth=32)
 model.summary()
 
 # Compile model.
@@ -98,7 +103,7 @@ checkpoint_cb = keras.callbacks.ModelCheckpoint(
 early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_acc", patience=15)
 
 # Train the model, doing validation at the end of each epoch
-epochs = 10
+epochs = 30
 model.fit(
     train_dataset,
     validation_data=validation_dataset,
