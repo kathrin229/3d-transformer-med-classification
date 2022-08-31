243f22f17f3c4ef4f277a4e937d79f46b5c67a58
kathrin229 <kathrin.hartmann@online.de>
2022-08-31 14:17:13 +0000

    new attention schemes: different calculation for space_only and first implementation of space_limited

---
+++ attention_schemes.py
@@ -0,0 +1,81 @@
+def attentions_schemes(x):
+    if self.attention_type == 'space_limited':
+        init_cls_token = x[:,0,:].unsqueeze(1)
+        cls_token = init_cls_token.repeat(1, 4, 1) #(2x2)
+        cls_token = rearrange(cls_token, 'b t m -> (b t) m',b=B,t=4).unsqueeze(1) #(2x2)
+        xt = x[:,1:,:]
+        xt = rearrange(xt, 'b (h w t) m -> b h w t m',b=B,h=H,w=W,t=T)
+        xt = rearrange(xt, 'b (h1 h) (w1 w) t m -> (b h1 w1) (h w t) m',b=B,t=T, h1=2, w1=2) #h1=4, w1=5
+        xt = torch.cat((cls_token, xt), 1)
+        res_temporal = self.drop_path(self.attn(self.norm1(xt)))
+
+        cls_token = res_temporal[:,0,:]
+        cls_token = rearrange(cls_token, '(b t) m -> b t m',b=B,t=4) #(2x2)
+        cls_token = torch.mean(cls_token,1,True) ## averaging for every stick
+
+        res_temporal = res_temporal[:,1:,:]
+        res_temporal = rearrange(res_temporal, '(b h1 w1) (h w t) m -> b (h1 h) (w1 w) t m',b=B,t=T, h1=2, w1=2,h=4,w=5)
+        res_temporal = rearrange(res_temporal, 'b h w t m -> b (h w t) m ',b=B,h=H,w=W,t=T)
+
+        x = x + torch.cat((cls_token, res_temporal), 1)
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+
+    elif self.attention_type == 'space_only_different_calculation':
+        init_cls_token = x[:,0,:].unsqueeze(1)
+        cls_token = init_cls_token.repeat(1, T, 1)
+        cls_token = rearrange(cls_token, 'b t m -> (b t) m',b=B,t=T).unsqueeze(1)
+        xs = x[:,1:,:]
+        xs = rearrange(xs, 'b (h w t) m -> (b t) (h w) m',b=B,h=H,w=W,t=T)
+        xs = torch.cat((cls_token, xs), 1)
+        res_spatial = self.drop_path(self.attn(self.norm1(xs)))
+
+        ### Taking care of CLS token
+        cls_token = res_spatial[:,0,:]
+        cls_token = rearrange(cls_token, '(b t) m -> b t m',b=B,t=T)
+        cls_token = torch.mean(cls_token,1,True) ## averaging for every frame
+
+        res_spatial = res_spatial[:,1:,:]
+        res_spatial = rearrange(res_spatial, '(b t) (h w) m -> b (h w t) m',b=B,h=H,w=W,t=T)
+        res = res_spatial
+        
+        # x = xt
+
+        ## Mlp
+        x = x + torch.cat((cls_token, res), 1) # !!! x +
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+
+    elif self.attention_type == 'divided_space_time':
+        ## Temporal
+        xt = x[:,1:,:]
+        xt = rearrange(xt, 'b (h w t) m -> (b h w) t m',b=B,h=H,w=W,t=T)
+        res_temporal = self.drop_path(self.temporal_attn(self.temporal_norm1(xt)))
+        res_temporal = rearrange(res_temporal, '(b h w) t m -> b (h w t) m',b=B,h=H,w=W,t=T)
+        res_temporal = self.temporal_fc(res_temporal)
+        xt = x[:,1:,:] + res_temporal
+
+        ## Spatial
+        init_cls_token = x[:,0,:].unsqueeze(1)
+        cls_token = init_cls_token.repeat(1, T, 1)
+        cls_token = rearrange(cls_token, 'b t m -> (b t) m',b=B,t=T).unsqueeze(1)
+        xs = xt
+        xs = rearrange(xs, 'b (h w t) m -> (b t) (h w) m',b=B,h=H,w=W,t=T)
+        xs = torch.cat((cls_token, xs), 1)
+        res_spatial = self.drop_path(self.attn(self.norm1(xs)))
+
+        ### Taking care of CLS token
+        cls_token = res_spatial[:,0,:]
+        cls_token = rearrange(cls_token, '(b t) m -> b t m',b=B,t=T)
+        cls_token = torch.mean(cls_token,1,True) ## averaging for every frame
+
+        res_spatial = res_spatial[:,1:,:]
+        res_spatial = rearrange(res_spatial, '(b t) (h w) m -> b (h w t) m',b=B,h=H,w=W,t=T)
+        res = res_spatial
+        
+        x = xt
+
+        ## Mlp
+        x = torch.cat((init_cls_token, x), 1) + torch.cat((cls_token, res), 1)
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
